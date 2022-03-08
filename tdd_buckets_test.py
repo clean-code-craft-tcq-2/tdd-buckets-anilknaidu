63af7f36447e761a9162a82a4780e939ae876320
Naidu Anil Kumar (RBEI/EPV32-EC) <AnilKumar.Naidu@in.bosch.com>
2022-03-08 21:45:32 +0530

    Test commit

--- tdd_buckets_test.py
+++ tdd_buckets_test.py
@@ -1,16 +1,16 @@
 import unittest
 from tdd_buckets import CurrentReadings
 
-currentReadingsTest = CurrentReadings("4,5")
+currentReadingsTest = CurrentReadings("4,5,6")
 
 class tdd_buckets_test(unittest.TestCase):
   def test_generateRangeFrequencyData(self):
     currentReadingsTest.generateRangeFrequencyData()
-    self.assertTrue(currentReadingsTest.CurrentRangeFrequencyDcn["4-5"]=="2")
+    self.assertTrue(currentReadingsTest.CurrentRangeFrequencyDcn["4-6"]=="3")
         
   def test_csv_data(self):
       currentReadingsTest.generateRangeFrequencyData()
-      self.assertTrue(currentReadingsTest.exportCSV()=="Range, Readings\n4-5,2\n")
+      self.assertTrue(currentReadingsTest.exportCSV()=="Range, Readings\n4-6,3\n")
   
 if __name__ =="__main__":
     unittest.main()
